c548216687095b8749e78cbebff35cb828e2a77d
Roger González <rogergonzalez21@gmail.com>
2015-06-29 14:41:05 -0430

    Converted .json to dict

--- new_york/views.py
+++ new_york/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json, os
 
 # Create your views here.
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def index(request):
+	
+	json_data = open(os.path.join(base_dir, 'static/data.json')).read()
+	json_dict = json.loads(json_data)
+	
 	return HttpResponse("Hello world!")
