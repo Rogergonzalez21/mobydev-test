0459bbc17fd9cf00a17c1d40b98d0574b0ad386c
Roger González <rogergonzalez21@gmail.com>
2015-06-29 15:17:54 -0430

    Not converting json to dict, using JsonResponse instead

--- new_york/views.py
+++ new_york/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
 import json, os
 
 # Create your views here.
@@ -7,7 +8,6 @@ base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def index(request):
 	
-	json_data = open(os.path.join(base_dir, 'static/data.json')).read()
-	json_dict = json.loads(json_data)
-	
-	return HttpResponse("Hello world!")
+	json_open = open(os.path.join(base_dir, 'static/data.json')).read()
+	json_data = JsonResponse(json_open, safe=False)
+	return render(request, 'index.html')
